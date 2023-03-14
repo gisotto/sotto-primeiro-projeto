442e7aff7b58065a0062aa7ae51b83e4fb4347a4
gisotto <gi@sotto.com.br>
2023-03-14 20:55:10 -0300

    meu primeiro e segundo component

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-meu-primeiro',
+    template: ` 
+        <p>Meu primeiro component com Angular2</p>
+    `,
+    styleUrls: ['./meu-primeiro.component.css']
+})
+ export class MeuPrimeiroComponent { }
