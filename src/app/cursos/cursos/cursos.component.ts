84c8e7a60b0bd3a6eb216a3416d168e82995ed6e
gisotto <gi@sotto.com.br>
2023-03-21 21:28:10 -0300

    Exercícios- Template

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+interface ICursos {
+  nome: string;
+  valor: number;
+}
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent {
+
+  public nomePortal: string = 'http://loiane.training';
+  public cursos: ICursos[] = [
+    { nome: 'Java', valor: 6 },
+    { nome: 'Ext Js', valor: 2 },
+    { nome: 'Angular', valor: 8 }
+  ];
+
+  constructor() {
+  }
 
 }
